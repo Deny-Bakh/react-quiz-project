7530066205ae98b5e83feded8bed78e54b43fd8c
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-09-13 18:16:29 +0300

    work in progress

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -36,49 +36,89 @@
 
 // export default QuizPage;
 
+// import React, { useState } from "react";
+// import { useParams } from "react-router-dom";
+// import QuizQuestion from "../../components/QuizQuestion/QuizQuestion";
+// import QuestionData from "../../components/QuizQuestion/QuestionData";
+
+// const QuizPage = () => {
+//     const { id } = useParams();
+//     const quizData = QuestionData[id];
+
+//     const initialScore = 0;
+//     const initialCurrentQuestion = 0;
+
+//     const [score, setScore] = useState(initialScore);
+//     const [currentQuestion, setCurrentQuestion] = useState(initialCurrentQuestion);
+  
+//     const handleAnswer = (isCorrect) => {
+//       if (isCorrect) {
+//         setScore(score + 1);
+//       }
+//       if (currentQuestion < quizData.length - 1) {
+//         setCurrentQuestion(currentQuestion + 1);
+//       } else {
+//         // Quiz is over, you can navigate to a result page or handle it here
+//       }
+//     };
+  
+//     return (
+//       <div>
+//         <h1>{quizData[0].question}</h1>
+//         <QuizQuestion
+//           QuestionData={quizData[currentQuestion]}
+//           onAnswer={handleAnswer}
+//           currentQuestion={currentQuestion + 1}
+//           totalQuestions={quizData.length}
+//         />
+//       </div>
+//     );
+//   };
+
+//   export default QuizPage;
+
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import QuizQuestion from "../../components/QuizQuestion/QuizQuestion";
 import QuestionData from "../../components/QuizQuestion/QuestionData";
+import QuizSelection from "../../components/QuizSelection/QuizSelection";
 
 const QuizPage = () => {
-    const { id } = useParams();
-    const quizData = QuestionData[id];
+  const { id } = useParams();
+  const quizData = QuestionData[id];
 
-    const initialScore = 0;
-    const initialCurrentQuestion = 0;
-  
-    // Check if quizData is undefined or empty
-    if (!quizData || quizData.length === 0) {
-      return <p>Quiz not found or data is empty.</p>;
+  const initialScore = 0;
+  const initialCurrentQuestion = 0;
+
+  const [score, setScore] = useState(initialScore);
+  const [currentQuestion, setCurrentQuestion] = useState(initialCurrentQuestion);
+
+  const handleAnswer = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
     }
-  
-    const [score, setScore] = useState(initialScore);
-    const [currentQuestion, setCurrentQuestion] = useState(initialCurrentQuestion);
-  
-    const handleAnswer = (isCorrect) => {
-      if (isCorrect) {
-        setScore(score + 1);
-      }
-      if (currentQuestion < quizData.length - 1) {
-        setCurrentQuestion(currentQuestion + 1);
-      } else {
-        // Quiz is over, you can navigate to a result page or handle it here
-      }
-    };
-  
-    return (
-      <div>
-        <h1>{quizData[0].question}</h1>
-        <QuizQuestion
-          QuestionData={quizData[currentQuestion]}
-          onAnswer={handleAnswer}
-          currentQuestion={currentQuestion + 1}
-          totalQuestions={quizData.length}
-        />
-      </div>
-    );
+    if (currentQuestion < quizData.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      // Quiz is over, you can navigate to a result page or handle it here
+    }
   };
 
-  export default QuizPage;
+  return (
+    <div>
+      <h1>{quizData[0].question}</h1>
+      <QuizQuestion
+        QuestionData={quizData[currentQuestion]}
+        onAnswer={handleAnswer}
+        currentQuestion={currentQuestion + 1}
+        totalQuestions={quizData.length}
+      />
+      {/* Include the QuizSelection component here */}
+      <QuizSelection />
+    </div>
+  );
+};
+
+export default QuizPage;
 
