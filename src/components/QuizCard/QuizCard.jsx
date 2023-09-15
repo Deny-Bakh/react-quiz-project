7d14b691e2e61d7dd7675ba85e95e3e8ea80d5e0
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-09-15 08:35:49 +0300

    classes

--- src/components/QuizCard/QuizCard.jsx
+++ src/components/QuizCard/QuizCard.jsx
@@ -1,176 +1,5 @@
-// import React, { useState } from "react";
-// import StartQuiz from "../StartQuiz/StartQuiz";
-// import {
-//   CardWrapper,
-//   Card,
-//   QuizImage,
-//   BackgroundColor,
-//   Name,
-//   Description,
-//   Button,
-//   Flex,
-// } from "./styled";
-// import QuizModal from "../QuizModal/QuizModal";
-
-// export default function QuizCard({ quiz }) {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   const handleShowMore = () => {
-//     setIsModalOpen(true);
-//   };
-
-//   const handleCloseModal = () => {
-//     setIsModalOpen(false);
-//   };
-
-//   return (
-//     <CardWrapper>
-//       <Card style={{ backgroundImage: `url(${quiz.background})` }}>
-//         <QuizImage src={quiz.image} alt={quiz.name} />
-//         <BackgroundColor>
-//           <Name>{quiz.quiz}</Name>
-//           <Description>{quiz.description.slice(0, 50)}...</Description>
-//         </BackgroundColor>
-//         <Flex>
-//           <Button type="button" onClick={StartQuiz}>Start Quiz</Button>
-//           <Button type="button" onClick={handleShowMore}>
-//             Show More
-//           </Button>
-//         </Flex>
-//       </Card>
-//       {isModalOpen && (
-//         <QuizModal quiz={quiz} onClose={handleCloseModal} />
-//       )}
-//     </CardWrapper>
-//   );
-// }
-
-// import React, { Component } from "react";
-// import StartQuiz from "../StartQuiz/StartQuiz";
-// import {
-//   CardWrapper,
-//   Card,
-//   QuizImage,
-//   BackgroundColor,
-//   Name,
-//   Description,
-//   Button,
-//   Flex,
-// } from "./styled";
-// import QuizModal from "../QuizModal/QuizModal";
-
-// class QuizCard extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isModalOpen: false,
-//     };
-//   }
-
-//   handleShowMore = () => {
-//     this.setState({ isModalOpen: true });
-//   };
-
-//   handleCloseModal = () => {
-//     this.setState({ isModalOpen: false });
-//   };
-
-//   render() {
-//     const { quiz } = this.props;
-//     const { isModalOpen } = this.state;
-
-//     return (
-//       <CardWrapper>
-//         <Card style={{ backgroundImage: `url(${quiz.background})` }}>
-//           <QuizImage src={quiz.image} alt={quiz.name} />
-//           <BackgroundColor>
-//             <Name>{quiz.quiz}</Name>
-//             <Description>{quiz.description.slice(0, 50)}...</Description>
-//           </BackgroundColor>
-//           <Flex>
-//             <Button type="button" onClick={StartQuiz}>
-//               Start Quiz
-//             </Button>
-//             <Button type="button" onClick={this.handleShowMore}>
-//               Show More
-//             </Button>
-//           </Flex>
-//         </Card>
-//         {isModalOpen && (
-//           <QuizModal quiz={quiz} onClose={this.handleCloseModal} />
-//         )}
-//       </CardWrapper>
-//     );
-//   }
-// }
-
-// export default QuizCard;
-
-// import React, { Component } from "react";
-// import {
-//   CardWrapper,
-//   Card,
-//   QuizImage,
-//   BackgroundColor,
-//   Name,
-//   Description,
-//   Button,
-//   Flex,
-// } from "./styled";
-// import QuizModal from "../QuizModal/QuizModal";
-// import StartQuiz from "../StartQuiz/StartQuiz";
-
-// class QuizCard extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isModalOpen: false,
-//     };
-
-//     this.handleShowMore = this.handleShowMore.bind(this);
-//     this.handleCloseModal = this.handleCloseModal.bind(this);
-//   }
-
-//   handleShowMore() {
-//     this.setState({ isModalOpen: true });
-//   }
-
-//   handleCloseModal() {
-//     this.setState({ isModalOpen: false });
-//   }
-
-//   render() {
-//     const { quiz } = this.props;
-//     const { isModalOpen } = this.state;
-
-//     return (
-//       <CardWrapper>
-//         <Card style={{ backgroundImage: `url(${quiz.background})` }}>
-//           <QuizImage src={quiz.image} alt={quiz.name} />
-//           <BackgroundColor>
-//             <Name>{quiz.quiz}</Name>
-//             <Description>{quiz.description.slice(0, 50)}...</Description>
-//           </BackgroundColor>
-//           <Flex>
-//             <Button type="button" onClick={StartQuiz}>Start Quiz</Button>
-//             <Button type="button" onClick={this.handleShowMore}>
-//               Show More
-//             </Button>
-//           </Flex>
-//         </Card>
-//         {isModalOpen && (
-//           <QuizModal quiz={quiz} onClose={this.handleCloseModal} />
-//         )}
-//       </CardWrapper>
-//     );
-//   }
-// }
-
-// export default QuizCard;
-
-
-import React, { Component } from "react";
-import StartQuiz from "../../components/StartQuiz/StartQuiz";
+import React, { Component } from 'react';
+import StartQuiz from '../StartQuiz/StartQuiz';
 import {
   CardWrapper,
   Card,
@@ -180,8 +9,8 @@ import {
   Description,
   Button,
   Flex,
-} from "./styled";
-import QuizModal from "../QuizModal/QuizModal";
+} from './styled';
+import QuizModal from '../QuizModal/QuizModal';
 
 class QuizCard extends Component {
   constructor(props) {
@@ -208,8 +37,8 @@ class QuizCard extends Component {
   }
 
   render() {
-    const { quiz } = this.props;
     const { isModalOpen } = this.state;
+    const { quiz } = this.props;
 
     return (
       <CardWrapper>
@@ -217,7 +46,10 @@ class QuizCard extends Component {
           <QuizImage src={quiz.image} alt={quiz.name} />
           <BackgroundColor>
             <Name>{quiz.quiz}</Name>
-            <Description>{quiz.description.slice(0, 50)}...</Description>
+            <Description>
+              {quiz.description.slice(0, 50)}
+              ...
+            </Description>
           </BackgroundColor>
           <Flex>
             <Button type="button" onClick={this.handleStartQuiz}>
@@ -238,6 +70,78 @@ class QuizCard extends Component {
 
 export default QuizCard;
 
+// import React, { Component } from "react";
+// import StartQuiz from "../StartQuiz/StartQuiz";
+// import {
+//   CardWrapper,
+//   Card,
+//   QuizImage,
+//   BackgroundColor,
+//   Name,
+//   Description,
+//   Button,
+//   Flex,
+// } from "./styled";
+// import QuizModal from "../QuizModal/QuizModal";
+
+// class QuizCard extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isModalOpen: false,
+//     };
+
+//     this.handleShowMore = this.handleShowMore.bind(this);
+//     this.handleCloseModal = this.handleCloseModal.bind(this);
+//     this.handleStartQuiz = this.handleStartQuiz.bind(this);
+
+//     this.quiz = props.quiz;
+//   }
+
+//   handleShowMore() {
+//     this.setState({ isModalOpen: true });
+//   }
+
+//   handleCloseModal() {
+//     this.setState({ isModalOpen: false });
+//   }
+
+//   handleStartQuiz() {
+//     StartQuiz();
+//   }
+
+//   render() {
+//     const { isModalOpen } = this.state;
+
+//     return (
+//       <CardWrapper>
+//         <Card style={{ backgroundImage: `url(${this.quiz.background})` }}>
+//           <QuizImage src={this.quiz.image} alt={this.quiz.name} />
+//           <BackgroundColor>
+//             <Name>{this.quiz.quiz}</Name>
+//             <Description>
+//               {this.quiz.description.slice(0, 50)}
+//               ...
+//             </Description>
+//           </BackgroundColor>
+//           <Flex>
+//             <Button type="button" onClick={this.handleStartQuiz}>
+//               Start Quiz
+//             </Button>
+//             <Button type="button" onClick={this.handleShowMore}>
+//               Show More
+//             </Button>
+//           </Flex>
+//         </Card>
+//         {isModalOpen && (
+//           <QuizModal quiz={this.quiz} onClose={this.handleCloseModal} />
+//         )}
+//       </CardWrapper>
+//     );
+//   }
+// }
+
+// export default QuizCard;
 
 // import React, { Component } from "react";
 // import StartQuiz from "../../components/StartQuiz/StartQuiz";
--- src/pages/quizes/Quizes.jsx
+++ src/pages/quizes/Quizes.jsx
@@ -1,13 +1,16 @@
-import React, { Component } from "react";
-import { quizes } from "../../api/quizes/quizes";
-import { CardContainer } from "./styled";
-import Card from "../../components/QuizCard/QuizCard";
+import React, { Component } from 'react';
+import { quizes } from '../../api/quizes/quizes';
+import { CardContainer } from './styled';
+import Card from '../../components/QuizCard/QuizCard';
 
 class Quizes extends Component {
-  state = {
-    quizzes: [],
-    error: '',
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      quizzes: [],
+      error: '',
+    };
+  }
 
   componentDidMount() {
     this.fetchQuizList();
@@ -22,12 +25,6 @@ class Quizes extends Component {
       this.setState({ error: err });
     }
   }
-
-  // componentDidUpdate(prevProps) {
-  //   if (prevProps.someProp !== this.props.someProp) {
-  //     this.fetchQuizList();
-  //   }
-  // }
 
   render() {
     const { quizzes, error } = this.state;
@@ -45,6 +42,54 @@ class Quizes extends Component {
 }
 
 export default Quizes;
+
+// import React, { Component } from "react";
+// import { quizes } from "../../api/quizes/quizes";
+// import { CardContainer } from "./styled";
+// import Card from "../../components/QuizCard/QuizCard";
+
+// class Quizes extends Component {
+//   state = {
+//     quizzes: [],
+//     error: '',
+//   };
+
+//   componentDidMount() {
+//     this.fetchQuizList();
+//   }
+
+//   async fetchQuizList() {
+//     try {
+//       const response = await quizes.get();
+//       this.setState({ quizzes: response, error: '' });
+//     } catch (err) {
+//       console.log(err);
+//       this.setState({ error: err });
+//     }
+//   }
+
+//   componentDidUpdate(prevProps) {
+//     if (prevProps.someProp !== this.props.someProp) {
+//       this.fetchQuizList();
+//     }
+//   }
+
+//   render() {
+//     const { quizzes, error } = this.state;
+
+//     if (error) return <p>{error}</p>;
+
+//     return (
+//       <CardContainer>
+//         {quizzes.map((quiz) => (
+//           <Card key={quiz.id} quiz={quiz} />
+//         ))}
+//       </CardContainer>
+//     );
+//   }
+// }
+
+// export default Quizes;
 
 // import React, { Component } from "react";
 // import { quizes } from "../../api/quizes/quizes";
